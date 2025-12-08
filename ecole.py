a4a28c9224be92b11e85965861972c6fc8812e7d
vivgxojo <maryse.mongeau@cegepoutaouais.qc.ca>
2025-12-08 13:19:34 -0500

    Solution bulletins v2

--- ecole.py
+++ ecole.py
@@ -2,47 +2,57 @@
 #TODO : d'entrer des noms d'élèves, des matière et des notes
 #TODO : et enregister les bulletins dans un ou des fichiers
 #TODO : on doit avoir plusieurs notes par étudiant par matière
-"""
-bulletins = {
-    "Jo": {
-        "Math": [12, 14, 18],
-        "Français": [15, 11, 16],
-        "Histoire": [17, 10, 19]
-    }
-    "Aline": {
-        ...
-    }
-}
-"""
-# bulletin = {}
-# while true
-    # demander prenom
-    # bulletin[prenom] = {}
-    # while true
-        # demander matière
-        # bulletin[prenom][matière] = []
-        #while true
-            #demander note
-            #bulletin[prenom][matière].append(note)
 
-            # TODO : breaks
-# sauvegarder bulletin dans un fichier
-import json
-bulletin = {}
+"""
+exemple :
+bulletin = {
+    "élève" : ["Alice", "Bob", "Chloé"],
+    "français" : [[95, 100, 90], [80, 70, 75], [50, 60, 65],
+    "math" : [[95, 100, 90], [80, 70, 75], [50, 60, 65]
+}
+pseudocode :
+bulletin = {
+     "élève" : []
+}
+while true
+    nom = quel élève?
+    bulletin[eleve].append(nom)
+    while true
+        matiere = quelle matière?
+        créer une clé matière dans le bulletin avec une liste vide imbriquée
+        while true
+            note = quelle note?
+            ajouter la note dans la liste imbriquée
+        conditions d'arrêts...
+sauvegarder le bulletin dans un fichier
+"""
+bulletin = {
+     "eleve" : []
+}
+nb_eleves = 0
 while True:
-    prenom = input("Prenom: ")
-    if prenom == "":
+    nom = input("Quel élève? ")
+    if nom == "":
         break
-    bulletin[prenom] = {}
+    bulletin["eleve"].append(nom)
     while True:
-        matiere = input("Matière: ")
+        matiere = input("Quelle matière? ")
         if matiere == "":
             break
-        bulletin[prenom][matiere] = []
+        if not matiere in bulletin:
+            bulletin[matiere] = []
+        bulletin[matiere].append([]) # on veut juste ajouter une liste imbriquer dans la liste
         while True:
-            note = int(input("Note: ")) # TODO : ajouter la gestion d'erreur
-            if note == -1:
+            note = input("Quelle note? ")
+            if note == "":
                 break
-            bulletin[prenom][matiere].append(note)
-with open("ecole/ecole.json", "w", encoding="utf-8") as fichier:
-    fichier.write(json.dumps(bulletin, ensure_ascii=False, indent=4))
+            bulletin[matiere][nb_eleves].append(int(note))
+    nb_eleves += 1
+print(bulletin)
+with open("ecole/mon_bulletin.txt", "w") as f:
+    for i in range(len(bulletin["eleve"])):
+        f.write(bulletin["eleve"][i] + "\n")
+        for k, v in bulletin.items():
+            if k != "eleve":
+                f.write(k + " : " + str(v[i]) + "\n")
+        f.write("\n")
